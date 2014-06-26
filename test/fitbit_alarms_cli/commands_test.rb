9a3b4f8080fd24a90d82a9ba29dfe579d3930f0b
Ben Colon <ben@colon.com.fr>
2014-06-26 21:10:24 +0200

    Fixed commands tests

--- test/fitbit_alarms_cli/commands_test.rb
+++ test/fitbit_alarms_cli/commands_test.rb
@@ -22,12 +22,22 @@ class FitbitAlarmsCli::CommandsTest < MiniTest::Test
   end
 
   def test_add_command
+    stub_client_init
     FitbitAlarmsCli::Client.any_instance.expects(:add_alarm)
     @commands.add("12:00+02:00")
   end
 
   def test_remove_command
+    stub_client_init
     FitbitAlarmsCli::Client.any_instance.expects(:remove_alarm)
     @commands.remove("1234")
+  end
+
+  private
+
+  def stub_client_init
+    File.expects(:exists?).returns(true)
+    YAML.expects(:load_file).returns({ :consumer_key => 1234, :consumer_secret => 1234,
+                                       :token => 1234, :secret => 1234 })
   end
 end
